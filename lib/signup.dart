dd9b41618c40e940316631c0c56b6ca5df5eb0c8
LuckyYunicorn <lucky.parihar@yunicorn.in>
2025-09-23 13:12:54 +0530

    signup text

--- lib/signup.dart
+++ lib/signup.dart
@@ -10,6 +10,8 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text("data"),
+    );
   }
 }
